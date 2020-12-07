3fa1e25f5b7468d463c4167e5d59eef8fadab700
fatopato <fatih.koprucu@gmail.com>
2020-12-07 22:48:17 +0300

    yeni kurallar eklendi

--- src/main/java/com/javainuse/service/JewelleryShopService.java
+++ src/main/java/com/javainuse/service/JewelleryShopService.java
@@ -43,17 +43,17 @@ public class JewelleryShopService {
 	}
 
 	public void  generateCampaigns(){
-		this.eligibleCampaigns.add(new Campaign(1L, "%10 indirim kampanyası", "single", new Benefit(1L, "discount by percentage",  new Discount("percent", 10.0))));
-		this.eligibleCampaigns.add(new Campaign(2L,  "100TL indirim kampanyası", "single", new Benefit(2L, "discount by cash",  new Discount("cash", 100.0))));
+		this.eligibleCampaigns.add(new Campaign(1L, "%x indirim kampanyası", "single", new Benefit(1L, "discount by percentage",  new Discount("percent", 10.0))));
+		this.eligibleCampaigns.add(new Campaign(2L,  "xTL indirim kampanyası", "single", new Benefit(2L, "discount by cash",  new Discount("cash", 100.0))));
 
 		this.eligibleCampaigns.add(new Campaign(3L, "Seçili ürün kategorilerinde %15 indirim kampanyası", "single", new Benefit(3L, "discount by percentage",  new Discount("percent", 15.0))));
 		this.eligibleCampaigns.add(new Campaign(4L,  "Seçili ürün kategorilerinde 40TL indirim kampanyası", "single", new Benefit(4L, "discount by cash",  new Discount("cash", 40.0))));
 
-		this.eligibleCampaigns.add(new Campaign(5L, "Seçili üründe %10 indirim kampanyası", "single", new Benefit(5L, "discount by percentage",  new Discount("percent", 10.0))));
-		this.eligibleCampaigns.add(new Campaign(6L,  "Seçili üründe 20TL indirim kampanyası", "single", new Benefit(6L, "discount by cash",  new Discount("cash", 20.0))));
+		this.eligibleCampaigns.add(new Campaign(5L, "Seçili üründe %x indirim kampanyası", "single", new Benefit(5L, "discount by percentage",  new Discount("percent", 10.0))));
+		this.eligibleCampaigns.add(new Campaign(6L,  "Seçili üründe xTL indirim kampanyası", "single", new Benefit(6L, "discount by cash",  new Discount("cash", 20.0))));
 
-		this.eligibleCampaigns.add(new Campaign(7L, "Kredi kartı ödemelerinde % 25 indirim kampanyası", "single", new Benefit(7L, "discount by percentage",  new Discount("percent", 25.0))));
-		this.eligibleCampaigns.add(new Campaign(8L,  "Nakit ödemelerde 36TL indirim kampanyası", "single", new Benefit(8L, "discount by cash",  new Discount("cash", 36.0))));
+		this.eligibleCampaigns.add(new Campaign(7L, "Kredi kartı ödemelerinde %x indirim kampanyası", "single", new Benefit(7L, "discount by percentage",  new Discount("percent", 25.0))));
+		this.eligibleCampaigns.add(new Campaign(8L,  "Nakit ödemelerde xTL indirim kampanyası", "single", new Benefit(8L, "discount by cash",  new Discount("cash", 36.0))));
 
 		this.eligibleCampaigns.add(new Campaign(9L, "Seçili ürünlerde 40 puan indirim kampanyası", "single", new Benefit(9L, "discount by point",  new Discount("point", 40.0))));
 		this.eligibleCampaigns.add(new Campaign(10L,  "Seçili ürün kategorilerinde 50 puan indirim kampanyası", "single", new Benefit(10L, "discount by point",  new Discount("point", 50.0))));
